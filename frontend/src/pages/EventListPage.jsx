f4d6217c664d4fb3ba04416c51bfcc138b04f2f4
junesutan <juneowns@gmail.com>
2025-10-28 14:41:12 +0800

    added searchbar and button

--- frontend/src/pages/EventListPage.jsx
+++ frontend/src/pages/EventListPage.jsx
@@ -5,6 +5,35 @@ import Eventcard from "../components/Eventcard.jsx";
 export default function EventListPage() {
   return (
     <div>
+      {/* search bar */}
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "10px 0 20px 0",
+          width: "100%",
+          height: "38px",
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Search events"
+          style={{
+            maxWidth: "350px",
+            minWidth: "150px",
+            width: "100%",
+            height: "100%",
+          }}
+        ></input>
+        <button
+          style={{ height: "100%", padding: "0 16px", boxSizing: "border-box" }}
+        >
+          {" "}
+          🔍{" "}
+        </button>
+      </div>
       {/* list of events container */}
       <div
         style={{
