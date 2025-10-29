f85e8e794d9fd6aa45161a67c9b45fe43b7eecff
Jeremy Ang <jang025@users.noreply.github.com>
2025-10-29 14:24:26 +0800

    feat: protecting pages based on whether valid token exists or not

--- backend/middleware/isLoggedIn.js
+++ backend/middleware/isLoggedIn.js
@@ -2,13 +2,24 @@ const jwt = require("jsonwebtoken");
 
 const isLoggedIn = async (req, res, next) => {
   try {
+    // look for authorization header
     const authHeader = req.get("Authorization");
+    // if there is no header
+    if (!authHeader) {
+      res.status(401).json({ msg: "no token provided" });
+      return;
+    }
+
+    // extract token
     const token = authHeader.split(" ")[1];
+    // verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // makes user data accessible
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(403).json({ msg: "you cannot enter", error });
+    res.status(403).json({ msg: "invalid token", error });
+    return;
   }
 };
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,17 +10,31 @@ import ConfirmationPage from "./pages/ConfirmationPage";
 import EventListPage from "./pages/EventListPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import EditProfilePage from "./pages/EditProfilePage.jsx";
+import { useState } from "react";
 
 const App = () => {
-  // const [token, setToken] = useState(null);
+  const [token, setToken] = useState(null);
   // const [available, setAvailable] = useState(true)
   // const [ongoing, setOngoing] = useState(true)
+
+  if (token === null) {
+    return (
+      <main>
+        {/* <Navbar /> */}
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/eventlist" element={<EventListPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage setToken={setToken} />} />
+        </Routes>
+      </main>
+    );
+  }
   return (
     <main>
       {/* <NavBar /> */}
       <Routes>
         {/* <Route path="/" element={<HomePage />} /> */}
-        <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/book" element={<BookTicketPage />} />
         <Route path="/booking" element={<BookingPage />} />
